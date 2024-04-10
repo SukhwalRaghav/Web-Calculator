e4d853b55528c865b942593c0d9717a6a89bad43
Raghav Sukhwal <111757995+SukhwalRaghav@users.noreply.github.com>
2024-04-10 11:39:40 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,10 +1,23 @@
 let string = "";
+let count = 0;
 let buttons = document.querySelectorAll('.button');
 Array.from(buttons).forEach((button) => {
     button.addEventListener('click', (e) => {
         if(e.target.innerHTML == '='){
             string = eval(string);
             document.querySelector('input').value = string; 
+        }
+        else if(e.target.innerHTML == '-/+'){
+            if(count == 0){
+                count += 1;
+                string = "-" + string;
+                document.querySelector('input').value = string;
+            }
+            else if(count > 0){
+                count -= 1;
+                string = string.startsWith('-') ? string.slice(1) : string;
+                document.querySelector('input').value = string;
+            }
         }
         else if(e.target.innerHTML == 'AC' || e.target.innerHTML == 'C'){
             string = "";
